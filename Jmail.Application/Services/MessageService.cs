6a6ec562cc11b217eb6324064487dfc72dccecbe
Jakub Wojnowski <83953649+jakubWojnowski@users.noreply.github.com>
2023-05-06 17:38:56 +0200

    working smtp with ethernal

--- Jmail.Application/Services/MessageService.cs
+++ Jmail.Application/Services/MessageService.cs
@@ -7,6 +7,7 @@ using Jmail.Domain.Interfaces;
 using MailKit.Security;
 using Microsoft.Extensions.Configuration;
 using MimeKit;
+using MimeKit.Text;
 using SmtpClient = System.Net.Mail.SmtpClient;
 
 namespace Jmail.Application.Services;
@@ -26,15 +27,17 @@ public class MessageService : IMessageService
 
     public async Task SendMessage(SendMessageDto dto)
     {
-        var message = new MailMessage();
+        var message = new MimeMessage();
         var contentMessage = _mapper.Map<Message>(dto);
-        message.To.Add(dto.ReciptientEmail);
+        message.From.Add(MailboxAddress.Parse("dereck.gerhold@ethereal.email"));
+        message.To.Add(MailboxAddress.Parse(dto.ReciptientEmail));
         message.Subject = dto.Title;
-        message.Body = dto.Content;
+        message.Body = new TextPart(TextFormat.Html) { Text = dto.Content };
         await _messageRepository.SendMessage(contentMessage);
         using var smtpClient = new MailKit.Net.Smtp.SmtpClient();
-        await smtpClient.ConnectAsync(Configuration.GetSection("Host").Value, 587,SecureSocketOptions.StartTls);
-        await smtpClient.AuthenticateAsync(Configuration.GetSection("Username").Value,Configuration.GetSection("Password").Value);
+        await smtpClient.ConnectAsync("smtp.ethereal.email", 587,SecureSocketOptions.StartTls);
+        await smtpClient.AuthenticateAsync("dereck.gerhold@ethereal.email","eYF1gaVrBMDHyeTsVH");
         await smtpClient.SendAsync((MimeMessage)message);
+        await smtpClient.DisconnectAsync(true);
     }
 }
